d5742f46ede3cd14af2010c369c07d8df3823286
Loubeants Celestin <loubeants@gmail.com>
2019-06-04 15:17:45 -0400

    commented out pages

--- server/controllers/api/index.js
+++
@@ -1,6 +0,0 @@
-const apiControllers = require('express').Router();
-
-apiControllers.use('/users', require('./usersController'));
-apiControllers.use('/secrets', require('./secretsController'));
-
-module.exports = apiControllers;
--- server/controllers/index.js
+++
@@ -1,7 +0,0 @@
-const controllers = require('express').Router();
-
-const apiControllers = require('./api');
-
-controllers.use('/api', apiControllers);
-
-module.exports = controllers;
